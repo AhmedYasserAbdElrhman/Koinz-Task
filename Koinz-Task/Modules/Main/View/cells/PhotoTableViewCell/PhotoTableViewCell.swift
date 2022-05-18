a0af8ad30a46d98d89055f3a71ccdeac177ff571
Ahmed Yasser <biko.yasser@gmail.com>
2022-05-18 10:16:22 +0200

    Main View

---
+++ Koinz-Task/Modules/Main/View/cells/PhotoTableViewCell/PhotoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoTableViewCell.swift
+//  Koinz-Task
+//
+//  Created by Ahmad Yasser on 18/05/2022.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Koinz-Task/Modules/Main/View/vc/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  Koinz-Task
+//
+//  Created by Ahmad Yasser on 18/05/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

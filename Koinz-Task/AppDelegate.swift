9dc3e2a4590d6f959ef5f646c6a5a0a323bc554c
Ahmed Yasser <biko.yasser@gmail.com>
2022-05-18 10:10:39 +0200

    Initial Commit

---
+++ Koinz-Task/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Koinz-Task
+//
+//  Created by Ahmad Yasser on 18/05/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
